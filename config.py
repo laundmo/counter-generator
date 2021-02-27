52b96ede55ea0d961c414102762c6430275d9fb9
laundmo <laurinschmidt2001@gmail.com>
2021-02-27 08:59:40 +0100

    black formatting

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
 from sys import platform
+
 try:
-    from yaml import CSafeLoader as Loader # use the C loader when possible
+    from yaml import CSafeLoader as Loader  # use the C loader when possible
 except ImportError:
     from yaml import SafeLoader as Loader
 import yaml
 
 with open("config.yml") as f:
-    config = yaml.load(f, Loader=Loader) # load the config yaml
+    config = yaml.load(f, Loader=Loader)  # load the config yaml
 
 if platform in ("linux", "linux2", "win32"):
     import PySimpleGUI
-elif platform == "darwin": # Have to use web/remi on MacOS as the normal tkinter version causes a OS error 
+elif (
+    platform == "darwin"
+):  # Have to use web/remi on MacOS as the normal tkinter version causes a OS error
     # TODO: Test on MacOS with tkinter possibly figure out how to get it working.
     import PySimpleGUIWeb as PySimpleGUI
--- counter_renderer.py
+++ counter_renderer.py
@@ -15,16 +15,18 @@ def get_counter(n: int) -> str:
 
     Returns:
         str: the formatted counter string
-    """    
-    return config["output"]["prepend"] + str(round((n / 10) + config["output"]["start_at"], 1))
+    """
+    return config["output"]["prepend"] + str(
+        round((n / 10) + config["output"]["start_at"], 1)
+    )
 
 
 @dataclass
 class RenderConfig:
-    """Dataclass for storing the render config
-    """    
+    """Dataclass for storing the render config"""
+
     font: str
-    name_template: str 
+    name_template: str
     width: int
     height: int
     fps: int
@@ -34,19 +36,23 @@ class RenderConfig:
     start_at: int
 
 
-CounterRenderer_T = TypeVar('CounterRenderer_T', bound='CounterRenderer')
+CounterRenderer_T = TypeVar("CounterRenderer_T", bound="CounterRenderer")
+
 
 class CounterRenderer:
-    """Class that renders the counter"""    
+    """Class that renders the counter"""
+
     def __init__(self, keyframes: List[List[tuple]], **kwargs):
         """Create a CounterRenderer instance
 
         Args:
             keyframes (List[List[tuple]]): The keyframes to render
-        """        
+        """
         self.keyframes = keyframes
-        self.c = RenderConfig(**{**config["output"], **kwargs}) # create the RenderConfig based on the config values which are overwritten by kwargs to this class
-        self.font = ImageFont.truetype(self.c.font, self.c.fontsize) # load the font
+        self.c = RenderConfig(
+            **{**config["output"], **kwargs}
+        )  # create the RenderConfig based on the config values which are overwritten by kwargs to this class
+        self.font = ImageFont.truetype(self.c.font, self.c.fontsize)  # load the font
 
     @classmethod
     def from_file(cls, file: str, **kwargs) -> CounterRenderer_T:
@@ -57,7 +63,7 @@ class CounterRenderer:
 
         Returns:
             CounterRenderer_T: The CounterRenderer instance
-        """        
+        """
         with open(file) as f:
             keyframes = json.load(f)
         return cls(keyframes, **kwargs)
@@ -72,56 +78,77 @@ class CounterRenderer:
         Returns:
             np.ndarray: The frame which has been written on
         """
-        img_pil = Image.fromarray(frame) # get a PIL(low) image from the array
-        draw = ImageDraw.Draw(img_pil) # Get a drawing object
-        draw.text((0, 0), text, font=self.font, fill=(255, 255, 255, 0)) # Draw the text
+        img_pil = Image.fromarray(frame)  # get a PIL(low) image from the array
+        draw = ImageDraw.Draw(img_pil)  # Get a drawing object
+        draw.text(
+            (0, 0), text, font=self.font, fill=(255, 255, 255, 0)
+        )  # Draw the text
         return np.array(img_pil)
 
-    def render_clip(self, keyframes: List[tuple], i: int): # TODO: Split render_clip into multiple methods - too long
+    def render_clip(
+        self, keyframes: List[tuple], i: int
+    ):  # TODO: Split render_clip into multiple methods - too long
         """Render a single clip
 
         Args:
             keyframes (List[tuple]): The keyframes to render
             i (int): The index of the clip (for saving the file)
-        """        
+        """
         seconds = max(keyframes) - min(keyframes)
-        frames = np.linspace(0, seconds, num=round(self.c.fps * seconds)) # create a numpy array with all the frame times needed 
+        frames = np.linspace(
+            0, seconds, num=round(self.c.fps * seconds)
+        )  # create a numpy array with all the frame times needed
 
-        fourcc = cv2.VideoWriter_fourcc(*"MP42") # dont ask me why a MP42 fourcc is needed
-        video = cv2.VideoWriter( # create a video writer
-            f"./{self.c.name_template.format(i)}.avi", # with the correct filename
+        fourcc = cv2.VideoWriter_fourcc(
+            *"MP42"
+        )  # dont ask me why a MP42 fourcc is needed
+        video = cv2.VideoWriter(  # create a video writer
+            f"./{self.c.name_template.format(i)}.avi",  # with the correct filename
             fourcc,
             float(self.c.fps),
             (self.c.width, self.c.height),
         )
 
         keyframe_index = 0
-        for index, _ in enumerate(frames): # loop over all the frames, no need to get the actual value since we only need the next frame
-            frame = np.zeros((self.c.height, self.c.width, 3), dtype=np.uint8) # create a frame with zeroes - black frame
+        for index, _ in enumerate(
+            frames
+        ):  # loop over all the frames, no need to get the actual value since we only need the next frame
+            frame = np.zeros(
+                (self.c.height, self.c.width, 3), dtype=np.uint8
+            )  # create a frame with zeroes - black frame
             try:
-                diff_next = (keyframes[keyframe_index] - min(keyframes)) - frames[index + 1] # calculate the difference between the relative keyframe time since start and the next frame time since start
-                if diff_next < 0: # if thats smaller than 0 we have passed the keyframe
-                    keyframe_index += 1 # which means we have counted to the next
+                diff_next = (keyframes[keyframe_index] - min(keyframes)) - frames[
+                    index + 1
+                ]  # calculate the difference between the relative keyframe time since start and the next frame time since start
+                if diff_next < 0:  # if thats smaller than 0 we have passed the keyframe
+                    keyframe_index += 1  # which means we have counted to the next
             except IndexError:
                 pass
-            frame = self.write_on_frame(frame, get_counter(keyframe_index)) # write the counter on the frame
-            video.write(frame) # and write the frame on the video
+            frame = self.write_on_frame(
+                frame, get_counter(keyframe_index)
+            )  # write the counter on the frame
+            video.write(frame)  # and write the frame on the video
 
-        for _ in range(self.c.fps * self.c.extend): # add fps * extend number of frames at the end
-            frame = np.zeros((self.c.height, self.c.width, 3), dtype=np.uint8) # all filled with zeroes
-            frame = self.write_on_frame(frame, get_counter(keyframe_index)) # and the end count
+        for _ in range(
+            self.c.fps * self.c.extend
+        ):  # add fps * extend number of frames at the end
+            frame = np.zeros(
+                (self.c.height, self.c.width, 3), dtype=np.uint8
+            )  # all filled with zeroes
+            frame = self.write_on_frame(
+                frame, get_counter(keyframe_index)
+            )  # and the end count
             video.write(frame)
 
-        video.release() # make sure we save and close the video file handle
+        video.release()  # make sure we save and close the video file handle
 
     def render_individual_clips(self):
-        """Render the individual clips recorded to videos
-        """        
+        """Render the individual clips recorded to videos"""
         for i, frames in enumerate(self.keyframes):
             if len(frames) > 0:
                 self.render_clip(frames, i)
 
 
-if __name__ == "__main__": # test setup, for running this file directly
+if __name__ == "__main__":  # test setup, for running this file directly
     r = CounterRenderer([list(range(15))])
     r.render_individual_clips()
